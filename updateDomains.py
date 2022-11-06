3db43945a7f981fa51f456b9f71009968eeac060
antonydp <38143733+antonydp@users.noreply.github.com>
2022-11-06 22:56:03 +0100

    Update updateDomains.py

--- updateDomains.py
+++ updateDomains.py
@@ -4,27 +4,22 @@ import re
 import undetected_chromedriver as uc
 from lib import proxytranslate
 from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start() 
 from selenium import webdriver
 
 if __name__ == '__main__':
     
-    options = webdriver.ChromeOptions() 
-    options.add_argument("--no-sandbox")
-    options.add_argument("start-maximized")
-    chrome = uc.Chrome(options=options)
-
+    display = Display(visible=0, size=(800, 800))  
+    display.start() 
+    driver = uc.Chrome()
     fileJson = 'channels.json'
 
     with open(fileJson) as f:
         data = json.load(f)
 
-    
     for chann, host in sorted(data.items()):
-            chrome.get(host)
-            data[chann] = chrome.current_url
-            print(chrome.current_url)
+        driver.get(host)
+        data[chann] = driver.current_url
+        print(driver.current_url)
 
     # I write the updated file
     with open(fileJson, 'w') as f:
