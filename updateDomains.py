cb5e82065718f6ffa68a421fba3f6cb7aff961d0
antonydp <38143733+antonydp@users.noreply.github.com>
2022-11-06 12:50:50 +0100

    Update updateDomains.py

--- updateDomains.py
+++ updateDomains.py
@@ -81,47 +81,9 @@ if __name__ == '__main__':
 
     
     for chann, host in sorted(data.items()):
-            # to get an idea of the timing
-            # useful only if you control all channels
-            # for channels with error 522 about 40 seconds are lost ...
-            print("check #### INIZIO #### channel - host :%s - %s " % (chann, host))
-
-            rslt = http_Resp([host])
-
-            # all right
-            if rslt['code'] == 200:
-                data[chann] = host
-            # redirect
-            elif str(rslt['code']).startswith('3'):
-                # data[k][chann] = str(rslt['code']) +' - '+ rslt['redirect'][:-1]
-                data[chann] = rslt['redirect']
-            # cloudflare...
-            elif rslt['code'] in [429, 503, 403]:
-                print('Cloudflare riconosciuto')
-                try:
-                    a = re.search('<base href="([^"]+)', proxytranslate.process_request_proxy(host).get('data', '')).group(1)
-                    print("dw" + a)
-                    page_data = proxytranslate.process_request_proxy(host).get('data', '')
-                    data[chann] = re.search('<base href="([^"]+)', page_data).group(1)
-                    rslt['code_new'] = 200
-                except Exception as e:
-                    import traceback
-                    traceback.print_exc()
-            # non-existent site
-            elif rslt['code'] == -2:
-                print('Host Sconosciuto - '+ str(rslt['code']) +' - '+ host)
-            # site not reachable
-            elif rslt['code'] == 111:
-                driver.get(host)
-                data[chann] = driver.current_url
-            else:
-                # other types of errors
-                print('Errore Sconosciuto - '+str(rslt['code']) +' - '+ host)
-
-            print("check #### FINE #### rslt :%s  " % (rslt))
-            print(data[chann])
-            if data[chann].endswith('/'):
-                data[chann] = data[chann][:-1]
+            driver.get(host)
+            data[chann] = driver.current_url
+            print(driver.current_url)
 
     # I write the updated file
     with open(fileJson, 'w') as f:
