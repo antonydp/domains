8cfaa0c6cea8e8b80174269da4b20101b1c4fcdd
antonydp <38143733+antonydp@users.noreply.github.com>
2022-11-07 01:10:26 +0100

    Update updateDomains.py

--- updateDomains.py
+++ updateDomains.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
             if data[chann].startswith("https://www."):
                 data[chann] = "https://"+data[chann][12:]
             if data[chann].startswith("http://www."):
-                data[chann] = "http://"+data[chann][11:]
+                data[chann] = "https://"+data[chann][11:]
 
     # I write the updated file
     with open(fileJson, 'w') as f:
